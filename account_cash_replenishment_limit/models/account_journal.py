963ca59db0ba2cffb3ef64f200630f3ea6109d81
Andhitia Rama <andhitia.r@gmail.com>
2020-03-25 05:37:15 +0700

    [8.0.1.0.0] account_cash_replenishment_limit

---
+++ account_cash_replenishment_limit/models/account_journal.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class AccountJournal(models.Model):
+    _name = "account.journal"
+    _inherit = "account.journal"
+
+    replenishment_limit = fields.Float(
+        string="Replenishment Limit",
+        default=0.0,
+    )
