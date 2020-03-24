0e3142277124d3fd02521cf7744aaa398d7a1688
Andhitia Rama <andhitia.r@gmail.com>
2020-03-25 05:37:15 +0700

    [8.0.1.0.0] account_cash_replenishment_limit

---
+++ account_cash_replenishment_limit/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Cash Register Replenishment Limit",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_accountant",
+    ],
+    "data": [
+        "views/account_journal_views.xml",
+        "views/account_bank_statement_views.xml",
+    ],
+}
---
+++ account_cash_replenishment_limit/models/account_bank_statement.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class AccountBankStatement(models.Model):
+    _name = "account.bank.statement"
+    _inherit = "account.bank.statement"
+
+    @api.depends(
+        "balance_end_real",
+    )
+    @api.multi
+    def _compute_replenish_amount(self):
+        for document in self:
+            replenishment_amount = 0.0
+            if document.journal_id:
+                journal = document.journal_id
+                replenishment_amount = journal.replenishment_limit - \
+                    document.balance_end_real
+            document.replenish_amount = replenishment_amount
+
+    replenish_amount = fields.Float(
+        string="Replenishment Amount",
+        compute="_compute_replenish_amount",
+        store=True,
+    )
