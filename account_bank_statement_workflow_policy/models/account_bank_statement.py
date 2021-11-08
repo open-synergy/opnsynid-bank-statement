22210c1baac489401d03f74ebdc1bfff5ef04bee
mikevhe18 <viriyananda.michael@gmail.com>
2021-11-08 10:59:54 +0700

    [account_bank_statement_workflow_policy] 11.0.1.0.0

---
+++ account_bank_statement_workflow_policy/models/account_bank_statement.py
@@ -0,0 +1,36 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountBankStatement(models.Model):
+    _name = "account.bank.statement"
+    _inherit = [
+        "account.bank.statement",
+        "mixin.policy",
+    ]
+
+    @api.multi
+    def _compute_policy(self):
+        _super = super(AccountBankStatement, self)
+        _super._compute_policy()
+
+    # Policy Field
+    reconcile_ok = fields.Boolean(
+        string="Can Reconcile",
+        compute="_compute_policy",
+    )
+    validate_ok = fields.Boolean(
+        string="Can Validate",
+        compute="_compute_policy",
+    )
+
+    @api.onchange(
+        "journal_id",
+    )
+    def onchange_policy_template_id(self):
+        template_id = self._get_template_id()
+        for document in self:
+            document.policy_template_id = template_id
