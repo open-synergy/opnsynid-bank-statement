5c35d8851d476b4933c3dcd05d140f8e5acfa2fe
Nurazmi <azmimr67@gmail.com>
2021-11-09 12:41:18 +0700

    [11.0.1.0.0] account_bank_statement_multiple_approval Task T004267

---
+++ account_bank_statement_multiple_approval/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Bank Statement Multiple Approval",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "account",
+        # "account_accountant",
+        "account_cancel",
+        "ssi_multiple_approval_mixin",
+    ],
+    "data": [
+        "views/account_bank_statement_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ account_bank_statement_multiple_approval/models/account_bank_statement.py
@@ -0,0 +1,57 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountBankStatement(models.Model):
+    _name = "account.bank.statement"
+    _inherit = [
+        "account.bank.statement",
+        "mixin.multiple_approval",
+    ]
+    _approval_from_state = "open"
+    _approval_to_state = "confirm"
+    _approval_state = "approval"
+
+    state = fields.Selection(
+        selection_add=[
+            ("approval", "Waiting For Approval"),
+            ("cancel", "Cancelled"),
+            ("reject", "Rejected"),
+        ]
+    )
+
+    def action_confirm(self):
+        for document in self:
+            document.write({"state": "approval"})
+            document.action_request_approval()
+
+    def action_restart(self):
+        for document in self:
+            document.write({"state": "open"})
+
+    def action_cancel(self):
+        for document in self:
+            for line in document.line_ids:
+                if line.journal_entry_ids:
+                    line.button_cancel_reconciliation()
+            document.write({"state": "cancel"})
+
+    def action_approve_approval(self):
+        _super = super(AccountBankStatement, self)
+        _super.action_approve_approval()
+        for document in self:
+            if document.approved:
+                document.check_confirm_bank()
+                document.write({"state": "confirm"})
+
+    def action_reject_approval(self):
+        _super = super(AccountBankStatement, self)
+        _super.action_reject_approval()
+        for document in self:
+            if document.rejected:
+                for line in document.line_ids:
+                    if line.journal_entry_ids:
+                        line.button_cancel_reconciliation()
